628c466f20ef6aab8b76b03863e7fbd0d1112afb
EarthenSkyLaptop <24978329+EarthenSky@users.noreply.github.com>
2021-09-18 03:17:05 -0700

    added materials and fixed prims alg

--- TheBackrooms/Assets/GenerateMazeChunk.cs
+++ TheBackrooms/Assets/GenerateMazeChunk.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class GenerateMazeChunk : MonoBehaviour
 {
     public GameObject cube;
+    public GameObject floor;
     public const int width = 32;
     public const int height = 32;
 
-    List<bool> wallMap = new List<bool>();
+    public List<bool> wallMap = new List<bool>();
 
     private static float GetAt(List<float> list, int x, int y, int width=width) {
         return list[x + width * y];
@@ -22,18 +23,18 @@ public class GenerateMazeChunk : MonoBehaviour
     private static void SetAt(List<bool> list, int x, int y, bool set, int width=width) {
         list[x + width * y] = set;
     }
-    public static bool InMap(int x, int y) {
+    public static bool InMap(int x, int y, int width=width, int height=height) {
         return x > 0 && x < width && y > 0 && y < height;
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        Random.InitState(Mathf.FloorToInt(Mathf.PerlinNoise(transform.localPosition.x, transform.localPosition.y) * (float)int.MaxValue));
         List<float> map = new List<float>();
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                var nodeSeed = Mathf.PerlinNoise(transform.localPosition.x + x, transform.localPosition.y + y);
-                map.Add(nodeSeed);
+                map.Add(Random.Range(0, 1000));
             }
         }
 
@@ -44,59 +45,85 @@ public class GenerateMazeChunk : MonoBehaviour
         var startY = Mathf.FloorToInt(randomY * height);
         
         // Implement prim's algorithm
-        List<bool> set = new List<bool>();
+        List<bool> set = new List<bool>(); // this holds items in frontier
         for (int i = 0; i < width * height; i++) {
             set.Add(false);
         }
 
-        // init our actual map
+        // init our actual map -> this is our map with 
         for (int i = 0; i < width * 2 * height * 2; i++) {
             wallMap.Add(false);
         }
 
         List<(int, int)> frontier = new List<(int, int)>();
         frontier.Add((startX, startY));
+        Debug.Log(frontier[0]);
+
         while (frontier.Count != 0) {
-            frontier.Sort();
-            (int, int) current = frontier[0];
-            frontier.RemoveAt(0);
+            // TODO: this sort is inefficient...
+            frontier.Sort((a, b) => GetAt(map, a.Item1, a.Item2).CompareTo(GetAt(map, b.Item1, b.Item2)));
+            (int, int) current = frontier[frontier.Count-1];
+            frontier.RemoveAt(frontier.Count-1);
 
+            // add to the actual map of walls & floors.
             (int, int) last = (-1, -1);
-            if (InMap(current.Item1 - 1, current.Item2) && GetAt(set, current.Item1 - 1, current.Item2) == true)
-                SetAt(set, current.Item1 - 1, current.Item2, true);
-            else if (InMap(current.Item1 + 1, current.Item2) && GetAt(set, current.Item1 + 1, current.Item2) == false)
-                SetAt(set, current.Item1 + 1, current.Item2, true);
-            else if (InMap(current.Item1, current.Item2 - 1) && GetAt(set, current.Item1, current.Item2 -1) == false)
-                SetAt(set, current.Item1, current.Item2 - 1, true);
-            else if (InMap(current.Item1, current.Item2 + 1) && GetAt(set, current.Item1, current.Item2 + 1) == false)
-                SetAt(set, current.Item1, current.Item2 + 1, true);
-
+            if (InMap(current.Item1 - 1, current.Item2) && GetAt(wallMap, (current.Item1 - 1)*2, (current.Item2)*2, width*2) == true)
+                last = (current.Item1 - 1, current.Item2);
+            else if (InMap(current.Item1 + 1, current.Item2) && GetAt(wallMap, (current.Item1 + 1)*2, (current.Item2)*2, width*2) == true)
+                last = (current.Item1 + 1, current.Item2);
+            else if (InMap(current.Item1, current.Item2 - 1) && GetAt(wallMap, (current.Item1)*2, (current.Item2 - 1)*2, width*2) == true)
+                last = (current.Item1, current.Item2 - 1);
+            else if (InMap(current.Item1, current.Item2 + 1) && GetAt(wallMap, (current.Item1)*2, (current.Item2 + 1)*2, width*2) == true)
+                last = (current.Item1, current.Item2 + 1);
+            
             // write the path
-            if (last.Item1 == current.Item1) {
-                SetAt(wallMap, current.Item1, (current.Item2 + last.Item2)/2, true, width*2);
+            if (last != (-1, -1)) {
+                if (GetAt(wallMap, (current.Item1 + last.Item1), (current.Item2 + last.Item2), width*2) == true) {
+                    Debug.Log("nooooo");
+                }
+                SetAt(wallMap, (current.Item1 + last.Item1), (current.Item2 + last.Item2), true, width*2);
             } else {
-                SetAt(wallMap, (current.Item1 + last.Item1)/2, current.Item2, true, width*2);
+                Debug.Log("fuck");
+                //Debug.Log(frontier);
             }
-            SetAt(wallMap, current.Item1, current.Item2, true, width*2);
+            SetAt(wallMap, current.Item1*2, current.Item2*2, true, width*2);
 
-            if (InMap(current.Item1 - 1, current.Item2) && GetAt(set, current.Item1 - 1, current.Item2) == false)
+            // add adjacent objects.
+            if (InMap(current.Item1 - 1, current.Item2) && GetAt(set, current.Item1 - 1, current.Item2) == false) {
                 SetAt(set, current.Item1 - 1, current.Item2, true);
+                frontier.Add((current.Item1 - 1, current.Item2));
+            }
 
-            if (InMap(current.Item1 + 1, current.Item2) && GetAt(set, current.Item1 + 1, current.Item2) == false)
+            if (InMap(current.Item1 + 1, current.Item2) && GetAt(set, current.Item1 + 1, current.Item2) == false) {
                 SetAt(set, current.Item1 + 1, current.Item2, true);
+                frontier.Add((current.Item1 + 1, current.Item2));
+            }
 
-            if (InMap(current.Item1, current.Item2 - 1) && GetAt(set, current.Item1, current.Item2 -1) == false)
+            if (InMap(current.Item1, current.Item2 - 1) && GetAt(set, current.Item1, current.Item2 -1) == false) {
                 SetAt(set, current.Item1, current.Item2 - 1, true);
+                frontier.Add((current.Item1, current.Item2 - 1));
+            }
 
-            if (InMap(current.Item1, current.Item2 + 1) && GetAt(set, current.Item1, current.Item2 + 1) == false)
+            if (InMap(current.Item1, current.Item2 + 1) && GetAt(set, current.Item1, current.Item2 + 1) == false) {
                 SetAt(set, current.Item1, current.Item2 + 1, true);
+                frontier.Add((current.Item1, current.Item2 + 1));
+            }
         }
 
         GenerateMesh();
     }
 
     private void GenerateMesh() {
-
+        for (int y = 0; y < height * 2; y++) {
+            for (int x = 0; x < width * 2; x++) {
+                if (GetAt(wallMap, x, y, width*2)) {
+                    GameObject o = GameObject.Instantiate(floor, new Vector3(transform.localPosition.x + x * 10, 0, transform.localPosition.y + y * 10), Quaternion.identity, gameObject.transform);
+                    //o.layer = LayerMask.NameToLayer("groundLayer");
+                } else {
+                    GameObject.Instantiate(cube, new Vector3(transform.localPosition.x + x * 10, 0, transform.localPosition.y + y * 10), Quaternion.identity, gameObject.transform);
+                }
+            }
+        }
     }
 
     // Update is called once per frame
