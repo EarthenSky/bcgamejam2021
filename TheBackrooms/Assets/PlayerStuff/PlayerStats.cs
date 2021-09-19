31eaa2a95917ecf5a5c9ab9958f8e88078bd3f73
EarthenSkyLaptop <24978329+EarthenSky@users.noreply.github.com>
2021-09-19 01:40:25 -0700

    added grape & heal

--- TheBackrooms/Assets/PlayerStuff/PlayerStats.cs
+++ TheBackrooms/Assets/PlayerStuff/PlayerStats.cs
@@ -6,6 +6,9 @@ public class PlayerStats : MonoBehaviour
 {
     private int hp = 4;
 
+    public AudioSource grape;
+    public AudioSource sandwich;
+
     public Sprite face1;
     public Sprite face2;
     public Sprite face3;
@@ -33,9 +36,35 @@ public class PlayerStats : MonoBehaviour
         }
     }
 
+    void Heal(string foodname) {
+        hp += 1;
+
+        if (hp == 3)
+            current = face1;
+        else if (hp == 2)
+            current = face2;
+        else if (hp == 1)
+            current = face3;
+        else if (hp == 0)
+            current = dead;
+
+        overlay.GetComponent<SpriteRenderer>().sprite = current;
+
+        if (foodname == "grape") {
+            // todo: play mm is grape
+            grape.loop = false;
+            grape.Play();
+        } else {
+            // todo: play default
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        grape = transform.Find("Grape").GetComponent<AudioSource>();
+        sandwich = transform.Find("Sandwich").GetComponent<AudioSource>();
+
         overlay = new GameObject("overlay");
         overlay.AddComponent<SpriteRenderer>();
         overlay.transform.parent = transform.Find("Camera").transform;
