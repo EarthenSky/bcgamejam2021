6e84dac93bbaf4048e41e002d34c204a9d7654f7
EarthenSkyLaptop <24978329+EarthenSky@users.noreply.github.com>
2021-09-19 01:31:35 -0700

    player health added

--- TheBackrooms/Assets/PlayerStuff/PlayerStats.cs
+++ TheBackrooms/Assets/PlayerStuff/PlayerStats.cs
@@ -9,8 +9,11 @@ public class PlayerStats : MonoBehaviour
     public Sprite face1;
     public Sprite face2;
     public Sprite face3;
+    public Sprite dead;
 
     public Sprite current;
+
+    private GameObject overlay;
 
     void TakeDamage() {
         hp -= 1;
@@ -18,19 +21,33 @@ public class PlayerStats : MonoBehaviour
             current = face1;
         else if (hp == 2)
             current = face2;
-        else if (hp == 3)
+        else if (hp == 1)
             current = face3;
+        else if (hp == 0)
+            current = dead;
+
+        overlay.GetComponent<SpriteRenderer>().sprite = current;
+
+        if (hp == 0) {
+            // TODO: end the timer
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        overlay = new GameObject("overlay");
+        overlay.AddComponent<SpriteRenderer>();
+        overlay.transform.parent = transform.Find("Camera").transform;
+        overlay.transform.localPosition = new Vector3(0, 0, 0.31f);
+        overlay.transform.localScale = new Vector3(0.041f, 0.041f, 0.05f);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.T)) {
+            TakeDamage();
+        }
     }
 }
