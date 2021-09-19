d530939f4313514c62f407787abfaf96175bf28a
jla673 <>
2021-09-19 11:19:01 -0700

    Update pathing.cs

--- TheBackrooms/Assets/pathfinding/pathing.cs
+++ TheBackrooms/Assets/pathfinding/pathing.cs
@@ -11,7 +11,6 @@ public class pathing : MonoBehaviour
     public GameObject self;
     private bool goal = false;
     public GameObject target;
-    public UnityEngine.AI.NavMeshAgent agent;
     public NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
